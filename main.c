5496c89a1603cf89473579370a348f74a35c3d1e
Eungyu <ekpark97.dev@gmail.com>
2022-08-08 12:04:13 +0900

    refactor: add func readFile test: valgrind test complete

--- main.c
+++ main.c
@@ -4,12 +4,14 @@
 #include <stdint.h>
 #include <arpa/inet.h>
 
+
 const int SECTOR_SIZE = 512;
 const int MAX_GPT_PARTITION = 128;
 const int GPT_PARTITION_ENTRY_OFFSET = SECTOR_SIZE * 2;
 const int GPT_PARTITION_ENTRY_SIZE = 128;
 const int GPT_PARTITION_ENTRY_TYPE_GUID_SIZE = 16;
 const int GPT_PARTITION_ENTRY_LBA_SIZE = 8;
+
 
 struct GPT_PARTITION_ENTRY{
     uint8_t PARTITION_TYPE_GUID[16];
@@ -20,70 +22,107 @@ struct GPT_PARTITION_ENTRY{
     uint8_t PARTITION_NAME[36];
 };
 
-int main(int argc, char* argv[]){
-    if (argc != 2){
-        printf("usage: %s [IMAGE_FILE]\n", argv[0]);
-        return 1;
-    }
 
-    FILE* fp = fopen(argv[1], "rb");
+uint8_t* readFile(const char* filePath){
+    FILE* fp = fopen(filePath, "rb");
     if (fp == NULL){
         printf("@fopen error\n");
-        return 1;
+        return NULL;
     }
 
     if (fseek(fp, 0L, SEEK_END) != 0){
         printf("@fseek error\n");
-        return 1;
+        fclose(fp);
+        return NULL;
     }
 
     size_t sz = ftell(fp);
     if (sz == -1){
         printf("@ftell error\n");
-        return 1;
+        fclose(fp);
+        return NULL;
     }
     rewind(fp);
 
     uint8_t* buff = (uint8_t*)malloc(sizeof(uint8_t) * sz);
     if (buff == NULL){
         printf("@malloc error\n");
-        return 1;
+        fclose(fp);
+        return NULL;
     }
 
     size_t rsz = fread(buff, sizeof(uint8_t), sz, fp);
     if (rsz != sz){
         printf("@fread error\n");
+        free(buff);
+        buff = NULL;
+        fclose(fp);
+        return NULL;
+    }
+
+    fclose(fp);
+    return buff;
+}
+
+
+void free2DArray(uint8_t** ptr, size_t sz){
+    for(int _ = 0; _ < sz; _++){
+        free(ptr[_]);
+        ptr[_] = NULL;
+    }
+    free(ptr);
+    ptr = NULL;
+}
+
+
+int main(int argc, char* argv[]){
+    if (argc != 2){
+        printf("usage: %s [IMAGE_FILE]\n", argv[0]);
         return 1;
+    }
+
+    uint8_t* buff = readFile(argv[1]);
+    if (buff == NULL){
+        printf("@readFile error\n");
+        return -1;
     }
 
     uint8_t** partition_entries = (uint8_t**)malloc(sizeof(uint8_t**) * MAX_GPT_PARTITION);
     if (partition_entries == NULL){
-        printf("@malloc error\n");
+        printf("@malloc partition_entries error\n");
         return 1;
     }
-
+    
+    uint8_t* NO_GPT_PARTITION = (uint8_t*)calloc(GPT_PARTITION_ENTRY_TYPE_GUID_SIZE, sizeof(uint8_t));
     for (int _ = 0; _ < MAX_GPT_PARTITION; _++){
         partition_entries[_] = (uint8_t*)malloc(GPT_PARTITION_ENTRY_SIZE);
         if (partition_entries[_] == NULL){
-            printf("@malloc error\n");
+            printf("@malloc partition_entries[] error\n");
+            free2DArray(partition_entries, _);
+            free(NO_GPT_PARTITION);
             return 1;
         }
 
         size_t CURRENT_OFFSET = GPT_PARTITION_ENTRY_OFFSET + (_ * GPT_PARTITION_ENTRY_SIZE);
         struct GPT_PARTITION_ENTRY* CURRENT_GPT_PARTITION_ENTRY = (struct GPT_PARTITION_ENTRY*)malloc(GPT_PARTITION_ENTRY_SIZE);
         if (CURRENT_GPT_PARTITION_ENTRY == NULL){
-            printf("@malloc error\n");
+            printf("@malloc CURRENT_GPT_PARTITION_ENTRY error\n");
+            free2DArray(partition_entries, _);
+            free(NO_GPT_PARTITION);
             return 1;
         }
 
-        uint8_t* NO_GPT_PARTITION = (uint8_t*)calloc(GPT_PARTITION_ENTRY_TYPE_GUID_SIZE, sizeof(uint8_t));
         if (memcmp(&(buff[CURRENT_OFFSET]), NO_GPT_PARTITION, GPT_PARTITION_ENTRY_TYPE_GUID_SIZE) == 0){
+            free(CURRENT_GPT_PARTITION_ENTRY);
             continue;
         }
 
         memcpy(CURRENT_GPT_PARTITION_ENTRY, &(buff[CURRENT_OFFSET]), GPT_PARTITION_ENTRY_SIZE);
         if (CURRENT_GPT_PARTITION_ENTRY == NULL){
             printf("@memcpy error\n");
+            free2DArray(partition_entries, _);
+            free(CURRENT_GPT_PARTITION_ENTRY);
+            free(NO_GPT_PARTITION);
             return 1;
         }
         
@@ -99,15 +138,12 @@ int main(int argc, char* argv[]){
 
         printf(" %zu", LBA_REAL_ADDR);
         printf(" %zu\n", LBA_GAP);
+        free(CURRENT_GPT_PARTITION_ENTRY);
     }
 
-
-    for (int _ = 0; _ < MAX_GPT_PARTITION; _++){
-        free(partition_entries[_]);
-        partition_entries[_] = NULL;
-    }
-    free(partition_entries);
-    partition_entries = NULL;    
+    free2DArray(partition_entries, MAX_GPT_PARTITION);
+    free(NO_GPT_PARTITION);
+    free(buff);
 
     return 0;
 }
